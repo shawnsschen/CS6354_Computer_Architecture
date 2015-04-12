662514c7cf53eb209e1f1e335ceee3b89ccc5ef2
shawn <sschenshawn@gmail.com>
2015-04-12 03:36:07 -0400

    cuda program test script

---
+++ test.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a test script to test various sizes of input array when launching
+# cuda kernel code. It generates random numbers as the input vector size.
+# And it iterates 100 times to run the program.
+
+count=0
+while [ $count -lt 100 ]; do
+    size=`shuf -i 1-16000000 -n 1`
+    ./vector_max $size -k 1
+    let count+=1
+done
